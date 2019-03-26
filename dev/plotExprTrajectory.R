ed97ff73e45cd5e873897cffabdb2d275b25f372
Yoonsik Park <park.yoonsik@icloud.com>
2019-03-26 07:27:01 -0400

    A bit late

---
+++ dev/plotExprTrajectory.R
@@ -0,0 +1,50 @@
+# plotExprTrajectory.R
+
+#' Title.
+#'
+#' \code{plotExprTrajectory} Plots an iGraph of the p-Creod with coloring of the gene expression
+#'
+#' Details.
+#' @section Dependency: the pcreode package, which can be installed with:
+#' \code{sudo pip install pcreode}
+#'
+#' @param filename The filename of the .fcs flow cytometry file.
+#' @param overlayName The specific Gene/Protein in the .fcs file you would like to color on the trajectories
+#' @return
+#'
+#' @author Yoonsik Park (au)
+#'
+#' @examples
+#' # Loads the Myeloid Differentiation Flow Cyt. Dataset, and plots the CAR2 gene expression
+#' plotExprTrajectory("./data/rna_seq_myeloid.fcs", "CAR2")
+#'
+#' @export
+
+plotExprTrajectory <- function (filename, overlayName) {
+  if (! require(igraph, quietly=TRUE)) {
+    install.packages("igraph")
+  }
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  if (!requireNamespace("flowCore", quietly = TRUE))
+    BiocManager::install("flowCore", version = "3.8")
+  pre_data <- flowCore::read.FCS(filename)
+  write.csv(data.frame(pre_data@exprs), "./data/temp/temp.csv", row.names=F)
+  system(paste("/usr/local/bin/python ./dev/plotExprTrajectory.py ./data/temp/temp.csv ", overlayName, sep="", collapse = NULL))
+
+  m <- as.matrix(read.table("./data/temp/weights.txt"))
+  dens <- as.matrix(read.table("./data/temp/density.txt"))
+  nodes <- as.matrix(read.table("./data/temp/nodes.txt"))
+  colors <- as.matrix(read.table("./data/temp/colors.txt"))
+
+  ig <- igraph::graph.adjacency(m, mode="undirected", weighted=TRUE)
+  layout <- igraph::layout.kamada.kawai(ig, maxiter=10000)
+  graphCol = c()
+  for (i in 1:nrow(colors[,1:3])) {
+    graphCol = c(graphCol,colorspace::hex(colorspace::RGB(t(as.matrix(colors[i,3:1])))))
+  }
+  titleString = paste("p-Creode of Expression Data colored by ", overlayName, sep="", collapse = NULL)
+  plot(ig, vertex.size=dens, edge.width=2,vertex.color=graphCol, vertex.label=NA, layout=layout, main=titleString)
+}
+
+# [END]
---
+++ dev/plotExprTrajectory.py
@@ -0,0 +1,74 @@
+# imports the pcreode package
+import pcreode
+# pandas is a package used for making the handling of large data sets easier
+import pandas as pd
+# numpy is very common package for handling arrays and matrices
+import numpy as np
+from sklearn.decomposition import PCA as _PCA
+from sklearn.metrics import pairwise_distances
+from igraph import *
+import matplotlib
+from IPython.display import display, Image
+from sklearn import preprocessing
+
+import sys
+
+file_nm = sys.argv[1]
+
+data_raw = pd.read_csv( file_nm)
+overlay = data_raw[sys.argv[2]]
+data_pca = pcreode.PCA( data_raw)
+data_pca.get_pca()
+pca_reduced_data = data_pca.pca_set_components( 3)
+
+dens = pcreode.Density( pca_reduced_data)
+
+guess = dens.radius_best_guess()
+density_1 = dens.get_density( radius=guess)
+
+noise = 8.0
+target = 25.0
+
+downed, downed_ind = pcreode.Down_Sample( pca_reduced_data, density_1, noise, target)
+out_graph, out_ids = pcreode.pCreode( data=pca_reduced_data, density=density_1, noise=noise,
+                                      target=target, file_path="./data/temp/", num_runs=10)
+
+graph_ranks = pcreode.pCreode_Scoring( data=pca_reduced_data, file_path="./data/temp/", num_graphs=10)
+
+gid = graph_ranks[0]
+
+analysis = pcreode.Analysis( file_path="./data/temp/", graph_id=gid, data=pca_reduced_data, density=density_1, noise=noise)
+
+norm_dens = preprocessing.MinMaxScaler( feature_range=(2,8))
+dens = norm_dens.fit_transform( analysis._density.astype( float).reshape(-1, 1))[analysis.node_data_indices]
+
+seed = 1
+
+upper_range=3
+node_label_size=0
+
+w_adj = pcreode.return_weighted_adj( pca_reduced_data, "./data/temp/", gid)
+
+np.savetxt("./data/temp/weights.txt", w_adj)
+np.savetxt("./data/temp/density.txt", dens)
+np.savetxt("./data/temp/nodes.txt", analysis.node_data_indices)
+
+norm_ana = preprocessing.MinMaxScaler(feature_range=(0, upper_range))
+norm_ana.fit(overlay[analysis._density > analysis._noise].values.astype(np.float).reshape(-1, 1))
+old_ana = norm_ana.transform(overlay[analysis._density > analysis._noise].values.astype(np.float).reshape(-1, 1))
+# bin the data points to each node so that an average of closest surrounding nodes is used for overlay
+bin_dist = pairwise_distances(analysis.good_cells, analysis._data[analysis.node_data_indices])
+bin_assignments = np.argmin(bin_dist, axis=1)
+new_ana = overlay.values[analysis.node_data_indices]
+for ii in range(analysis.num_nodes):
+    new_ana[ii] = np.mean(old_ana[bin_assignments == ii])
+norm_1 = np.array(new_ana, dtype=float)
+cl_vals_1 = [[]] * analysis.num_nodes
+# colors to use for overlay
+get_cl = matplotlib.cm.get_cmap('RdYlBu_r')
+for jj in range(analysis.num_nodes):
+    cl_vals_1[jj] = get_cl(norm_1[jj])
+
+analysis.graph.vs["color"] = [cl_vals_1[kk] for kk in range(analysis.num_nodes)]
+
+np.savetxt("./data/temp/colors.txt", analysis.graph.vs["color"])
