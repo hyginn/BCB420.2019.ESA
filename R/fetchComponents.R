3102792a5b3abb4ab833e770760bd01ea3da3137
Nada Elnour <nada.elnour@mail.utoronto.ca>
2019-03-28 23:07:55 -0400

    removed merge markers

--- R/fetchComponents.R
+++ R/fetchComponents.R
@@ -15,33 +15,108 @@ fetchComponents <- function(sys) {
   # returns a fixed set of symbols.
   # Function stub for development purposes only.
 
-  message("Note: fetchComponents(...) is deprecated.",
-          "Use SyDBgetSysSymbols(<database>, <system code>) instead.",
-          sep = "\n")
+  message(
+    "Note: fetchComponents(...) is deprecated.",
+    "Use SyDBgetSysSymbols(<database>, <system code>) instead.",
+    sep = "\n"
+  )
 
-<<<<<<< HEAD
   if (sys == "PHALY") {
-=======
     if (sys == "PHALY") {
->>>>>>> fba11f7609182991780dc7e6c48d387ca623b626
-    s <- c("AMBRA1", "ATG14", "ATP2A1", "ATP2A2", "ATP2A3", "BECN1", "BECN2",
-           "BIRC6", "BLOC1S1", "BLOC1S2", "BORCS5", "BORCS6", "BORCS7",
-           "BORCS8", "CACNA1A", "CALCOCO2", "CTTN", "DCTN1", "EPG5", "GABARAP",
-           "GABARAPL1", "GABARAPL2", "HDAC6", "HSPB8", "INPP5E", "IRGM",
-           "KXD1", "LAMP1", "LAMP2", "LAMP3", "LAMP5", "MAP1LC3A", "MAP1LC3B",
-           "MAP1LC3C", "MGRN1", "MYO1C", "MYO6", "NAPA", "NSF", "OPTN",
-           "OSBPL1A", "PI4K2A", "PIK3C3", "PLEKHM1", "PSEN1", "RAB20", "RAB21",
-           "RAB29", "RAB34", "RAB39A", "RAB7A", "RAB7B", "RPTOR", "RUBCN",
-           "RUBCNL", "SNAP29", "SNAP47", "SNAPIN", "SPG11", "STX17", "STX6",
-           "SYT7", "TARDBP", "TFEB", "TGM2", "TIFA", "TMEM175", "TOM1",
-           "TPCN1", "TPCN2", "TPPP", "TXNIP", "UVRAG", "VAMP3", "VAMP7",
-           "VAMP8", "VAPA", "VPS11", "VPS16", "VPS18", "VPS33A", "VPS39",
-           "VPS41", "VTI1B", "YKT6")
-  } else {
-    s <- ""
-  }
+      s <-
+        c(
+          "AMBRA1",
+          "ATG14",
+          "ATP2A1",
+          "ATP2A2",
+          "ATP2A3",
+          "BECN1",
+          "BECN2",
+          "BIRC6",
+          "BLOC1S1",
+          "BLOC1S2",
+          "BORCS5",
+          "BORCS6",
+          "BORCS7",
+          "BORCS8",
+          "CACNA1A",
+          "CALCOCO2",
+          "CTTN",
+          "DCTN1",
+          "EPG5",
+          "GABARAP",
+          "GABARAPL1",
+          "GABARAPL2",
+          "HDAC6",
+          "HSPB8",
+          "INPP5E",
+          "IRGM",
+          "KXD1",
+          "LAMP1",
+          "LAMP2",
+          "LAMP3",
+          "LAMP5",
+          "MAP1LC3A",
+          "MAP1LC3B",
+          "MAP1LC3C",
+          "MGRN1",
+          "MYO1C",
+          "MYO6",
+          "NAPA",
+          "NSF",
+          "OPTN",
+          "OSBPL1A",
+          "PI4K2A",
+          "PIK3C3",
+          "PLEKHM1",
+          "PSEN1",
+          "RAB20",
+          "RAB21",
+          "RAB29",
+          "RAB34",
+          "RAB39A",
+          "RAB7A",
+          "RAB7B",
+          "RPTOR",
+          "RUBCN",
+          "RUBCNL",
+          "SNAP29",
+          "SNAP47",
+          "SNAPIN",
+          "SPG11",
+          "STX17",
+          "STX6",
+          "SYT7",
+          "TARDBP",
+          "TFEB",
+          "TGM2",
+          "TIFA",
+          "TMEM175",
+          "TOM1",
+          "TPCN1",
+          "TPCN2",
+          "TPPP",
+          "TXNIP",
+          "UVRAG",
+          "VAMP3",
+          "VAMP7",
+          "VAMP8",
+          "VAPA",
+          "VPS11",
+          "VPS16",
+          "VPS18",
+          "VPS33A",
+          "VPS39",
+          "VPS41",
+          "VTI1B",
+          "YKT6"
+        )
+    } else {
+      s <- ""
+    }
 
-  return(s)
+    return(s)
+  }
 }
 
 # [END]
