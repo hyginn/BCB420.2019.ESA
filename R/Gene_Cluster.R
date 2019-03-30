f5424a5a3a25a5ab3e5adc8a4789f7950cc79841
Kevin Lin <32544867+KevinJianLin@users.noreply.github.com>
2019-03-30 15:19:21 -0400

    make function to export

--- R/Gene_Cluster.R
+++ R/Gene_Cluster.R
@@ -14,24 +14,7 @@ if (!require(GEOquery, quietly=TRUE)) {
   biocLite("GEOquery")
   library(GEOquery)
 }
-# Load series and platform data from GEO
-raw_dataset_1 <- getGEO("GSE34512", GSEMatrix =TRUE, getGPL=FALSE)
-if (length(raw_dataset_1) > 1) {
-  index_of_raw_dataset_1 <- grep("GPL570", attr(raw_dataset_1, "names"))
-} else {
-  index_of_raw_dataset_1 <- 1
-}
-raw_dataset_1 <- raw_dataset_1[[index_of_raw_dataset_1]]
-# let us save file into directory.
-GSE34512 <- raw_dataset_1
-save(GSE34512, file="./data/GSE34512.RData")
-load(file="./data/GSE34512.RData")
-raw_dataset_1  <-GSE34512
-raw_dataset_1
-# Access contents via methods:
-featureNames(raw_dataset_1 )[1:10]
-sampleNames(raw_dataset_1 )
-exprs(raw_dataset_1)
+
 source("https://bioconductor.org/biocLite.R")
 biocLite("BiocUpgrade")
 if (!require(Biobase, quietly=TRUE)) {
@@ -48,81 +31,140 @@ library(outliers)
 # install package class for clustering purpose
 install.packages("class")
 library(class)
+install.packages("igraph")
+library("igraph")
+install.packages("network")
+library("network")
+install.packages("rgl")
+library("rgl")
+
+
+# Load series and platform data from GEO
+raw_dataset_1 <- getGEO("GSE34512", GSEMatrix =TRUE, getGPL=FALSE)
+if (length(raw_dataset_1) > 1) {
+  index_of_raw_dataset_1 <- grep("GPL570", attr(raw_dataset_1, "names"))
+} else {
+  index_of_raw_dataset_1 <- 1
+}
+raw_dataset_1 <- raw_dataset_1[[index_of_raw_dataset_1]] #load the dataset
+# let us save file into directory.
+GSE34512 <- raw_dataset_1 #save file into local directory
+save(GSE34512, file="./data/GSE34512.RData")
+load(file="./data/GSE34512.RData")
+raw_dataset_1  <-GSE34512
+raw_dataset_1
+# Access contents via methods:
+identifiername <- featureNames(raw_dataset_1 )
+featureNames(raw_dataset_1 )[1:nrow(raw_dataset_1)]
+
+sampleNames(raw_dataset_1 )
+exprs(raw_dataset_1)
+
 number_of_genes <- nrow(raw_dataset_1)
-number_of_genes
-number_of_samples <- ncol(raw_dataset_1)
-number_of_samples
+#row_2 <- min(50,row1<-round(number_of_genes/500))#since memory can only process certain amount of samples
+ #I tried to process of all of sampples and crashed
+
 # assign all of features and columns into an object;
-expression_dataset_1 <- raw_dataset_1[1:10,1:number_of_samples]
-expression_dataset_1
+expression_dataset_1 <- raw_dataset_1[1:row_2,1:ncol(raw_dataset_1)]
+
 # assign above object into a matrix using "exprs" function
-expression_dataset_1_Matrix <- matrix(exprs(expression_dataset_1), nrow=10,ncol=number_of_samples)
-expression_dataset_1_Matrix
-ncol(expression_dataset_1_Matrix)
-nrow(expression_dataset_1_Matrix)
-# omit the rows that has na value;
+expression_dataset_1_Matrix <- matrix(exprs(expression_dataset_1), nrow=nrow(raw_dataset_1),
+                                      ncol=ncol(raw_dataset_1))
+
 na.omit(expression_dataset_1_Matrix)
 ncol(expression_dataset_1_Matrix)
 nrow(expression_dataset_1_Matrix)
+
+# omit the rows that has na value;
+
+positively_correlated_genes <- function(expressionmatrix,identifiernames)
+{
+
+matrix_portion <- expressionmatrix[1:50,]
+identifiername_portion <- identifiernames[1:50]
+
 #lets transpose the matrix
-expression_dataset_1_Matrix_transpose <- t(expression_dataset_1_Matrix)
+expression_dataset_1_Matrix_transpose <- t(matrix_portion)
 expression_dataset_1_Matrix_transpose
+
 # let's remove the outliers
 rm.outlier(expression_dataset_1_Matrix_transpose)
-nrow1 <- nrow(expression_dataset_1_Matrix_transpose)
-nrow1
-ncol1 <- ncol(expression_dataset_1_Matrix_transpose)
-ncol1
+
 # create correlation matrix
 expression_dataset_1_Matrix_transpose_cor <- cor(expression_dataset_1_Matrix_transpose)
-expression_dataset_1_Matrix_transpose_cor
-threshold_value <- sequence(0.1:1)
+
 # Let's create adjancency matrix by choosing threhold value to 0.3.
-expression_dataset_1_Matrix_transpose_cor_adjancency_0.3 <- expression_dataset_1_Matrix_transpose_cor
+
+positively_correlated_matrix <- expression_dataset_1_Matrix_transpose_cor
 
 for (c in 1:nrow(expression_dataset_1_Matrix_transpose_cor))
 {
   for (d in 1:ncol(expression_dataset_1_Matrix_transpose_cor))
   {
-    if(expression_dataset_1_Matrix_transpose_cor[c,d] >= 0.3)
+    if(expression_dataset_1_Matrix_transpose_cor[c,d] >= 0.9)
     {
-      expression_dataset_1_Matrix_transpose_cor_adjancency_0.3[c,d] <-1
+      positively_correlated_matrix[c,d] <-1
     }
     else {
-      expression_dataset_1_Matrix_transpose_cor_adjancency_0.3[c,d] <-0
+      positively_correlated_matrix[c,d] <-0
     }
   }
 
 }
-for (c in 1:nrow(expression_dataset_1_Matrix_transpose_cor))
+coexpressed_genes<-graph_from_adjacency_matrix(positively_correlated_matrix
+                                               , mode="undirected",weighted=NULL)
+igraph_1 <- simplify(coexpressed_genes)
+return(largest_cliques(igraph_1))
+plot.igraph(igraph_1,vertex.color="red", vertex.size = 10,directed=FALSE)
+}
+
+
+positively_correlated_genes(expression_dataset_1_Matrix,identifiername)
+
+
+
+
+
+
+
+
+negatively_correlated_matrix <- expression_dataset_1_Matrix_transpose_cor
+for (e in 1:nrow(expression_dataset_1_Matrix_transpose_cor))
 {
-  for (d in 1:ncol(expression_dataset_1_Matrix_transpose_cor))
+  for (f in 1:ncol(expression_dataset_1_Matrix_transpose_cor))
   {
-    if(expression_dataset_1_Matrix_transpose_cor[c,d] >= 0.3)
+    if(expression_dataset_1_Matrix_transpose_cor[e,f] <= -0.3)
     {
-      expression_dataset_1_Matrix_transpose_cor_adjancency_0.3[c,d] <-1
+      negatively_correlated_matrix[e,f] <- 1
     }
 
-    else if(expression_dataset_1_Matrix_transpose_cor[c,d] <= -0.3)
+    else
     {
-      expression_dataset_1_Matrix_transpose_cor_adjancency_0.3[c,d] <- -1
+     negatively_correlated_matrix[e,f] <- 0
     }
   }
 
+  coexpressed_genes_1<-graph_from_adjacency_matrix(negatively_correlated_matrix
+                                                   , mode="undirected",weighted=NULL)
+  igraph_2 <- simplify(coexpressed_genes_1)
+  (largest_cliques(igraph_2))
+  plot.igraph(igraph_2,vertex.color="red", vertex.size = 10,directed=FALSE)
+
 }
+
+
+
 expression_dataset_1_Matrix_transpose_cor_adjancency_0.3
-install.packages("network")
-library("network")
+
 coexpressedgenes <- network(expression_dataset_1_Matrix_transpose_cor_adjancency_0.3, directed=FALSE)
 # plot the the network of these graph
 gene_name <- featureNames(raw_dataset_1 )[1:10]
 plot(coexpressedgenes, label=gene_name)
 #=======================Igraph ploting================================================================
-install.packages("igraph")
-library("igraph")
+
 # let's create an igrpah object from previous adjancency matrix.
 coexpressed_genes<-graph_from_adjacency_matrix(expression_dataset_1_Matrix_transpose_cor_adjancency_0.3
-                                               , mode="undirected",weighted=TRUE)
+                                               , mode="undirected",weighted=FALSE)
 igraph_1 <- simplify(coexpressed_genes)
 V(igraph_1)$name<-gene_name
 plot.igraph(igraph_1,vertex.color="red", vertex.size = 10,directed=FALSE)
@@ -131,8 +173,8 @@ plot.igraph(igraph_1,vertex.color="red", vertex.size = 10,directed=FALSE)
 #If you are running OS X, you need to install XQuartz which can be downlaod at https://www.xquartz.org,
 tkplot(igraph_1) # this is interactive 2D plotting.
 #if we want to see 3D ploting, we need to install "rgl" package
-intall.packages("rgl")
-library(rgl)
+
+
 tkplot(igraph_1)
 rglplot(igraph_1) #this will give you 3D plot
 #if we want to find out co-expressed genes, we can just enter gene name and find out the co-expressed
