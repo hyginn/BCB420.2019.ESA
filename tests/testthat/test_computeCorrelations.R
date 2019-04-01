7286b06650199f7dc9d7c935dcad9f7844952509
deni678 <denitsa.vasileva@mail.utoronto.ca>
2019-03-31 22:37:49 -0400

    changes

---
+++ tests/testthat/test_computeCorrelations.R
@@ -0,0 +1,12 @@
+library(testthat)
+# Correlation graphs test (takes ~3min).
+context("Correlation graphs test (takes ~170sec).")
+expected <- list(
+  "NLRIN" = list(recCount = 1225, corrSum = 145.111, goCorrSum = 674.092),
+  "SLIGR" = list(recCount = 1830, corrSum = 147.04, goCorrSum = 1056.275),
+  "PHALY" = list(recCount = 2145, corrSum = 101.0279, goCorrSum = 1348.659)
+)
+df <- BCB420.2019.ESA::computeCorrelations(TRUE)
+expect_equal(expected["PHALY"][[1]], calcStats(df, "PHALY"), tolerance = 0.01)
+expect_equal(expected["SLIGR"][[1]], calcStats(df, "SLIGR"), tolerance = 0.01)
+expect_equal(expected["NLRIN"][[1]], calcStats(df, "NLRIN"), tolerance = 0.01)
