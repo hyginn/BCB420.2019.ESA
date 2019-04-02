b35be8f8b5dee4817917c359bfc7bd60f9e22ac1
Matt <mtthwmcneil@gmail.com>
2019-04-02 02:27:45 -0400

    Modified functions for package changes

--- R/System_Expression.R
+++ R/System_Expression.R
@@ -50,32 +50,10 @@ exProf <- function(sym, myQNXP = myQNXP) {
   return(normalized)
 }
 
-fetchComponents <- function(sys) {
-  # returns a fixed set of symbols.
-  # Function stub for development purposes only.
-  if (sys == "PHALY") {
-    s <- c("AMBRA1", "ATG14", "ATP2A1", "ATP2A2", "ATP2A3", "BECN1", "BECN2",
-           "BIRC6", "BLOC1S1", "BLOC1S2", "BORCS5", "BORCS6", "BORCS7",
-           "BORCS8", "CACNA1A", "CALCOCO2", "CTTN", "DCTN1", "EPG5", "GABARAP",
-           "GABARAPL1", "GABARAPL2", "HDAC6", "HSPB8", "INPP5E", "IRGM",
-           "KXD1", "LAMP1", "LAMP2", "LAMP3", "LAMP5", "MAP1LC3A", "MAP1LC3B",
-           "MAP1LC3C", "MGRN1", "MYO1C", "MYO6", "NAPA", "NSF", "OPTN",
-           "OSBPL1A", "PI4K2A", "PIK3C3", "PLEKHM1", "PSEN1", "RAB20", "RAB21",
-           "RAB29", "RAB34", "RAB39A", "RAB7A", "RAB7B", "RPTOR", "RUBCN",
-           "RUBCNL", "SNAP29", "SNAP47", "SNAPIN", "SPG11", "STX17", "STX6",
-           "SYT7", "TARDBP", "TFEB", "TGM2", "TIFA", "TMEM175", "TOM1",
-           "TPCN1", "TPCN2", "TPPP", "TXNIP", "UVRAG", "VAMP3", "VAMP7",
-           "VAMP8", "VAPA", "VPS11", "VPS16", "VPS18", "VPS33A", "VPS39",
-           "VPS41", "VTI1B", "YKT6")
-  } else {
-    s <- ""
-  }
-  return(s)
-}
-
 System_Expression<- function(sysname, myQNXP){
+  sysDB <- fetchData("SysDB")
   # Get a list of genes present in the given system
-  components <- fetchComponents(sysname)
+  components <- SyDBgetSysSymbols(sysDB, sysname)[[1]]
 
   # Normalized expression values for each gene in the system
   expressions <- sapply(components, exProf, myQNXP)
@@ -83,9 +61,10 @@ System_Expression<- function(sysname, myQNXP){
   # Find the means and variance of the expression changes caused by each condition
   meanExpression <- rowMeans(expressions, na.rm = T)
   varExpression <- apply(expressions, 1, var, na.rm = T)
+  num_observations <- apply(expressions, 1, function(x) sum(!is.na(x)))
 
   # calculate the one-sided p-values for each experiment
-  p.values <- pnorm(meanExpression, sd = sqrt(varExpression))
+  p.values <- pnorm(meanExpression, sd = sqrt(varExpression/num_observations))
   p.values[p.values > 0.5] <- 1 - p.values[p.values > 0.5]
 
   # Find the experiments where the system was significantly up
