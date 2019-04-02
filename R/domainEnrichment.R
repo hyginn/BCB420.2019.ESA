ce0d126b657d4d8a998edf4c84660f751c310c5a
Van Shen <shenfan3698@gmail.com>
2019-04-02 16:31:44 -0400

    Update function and vignettes.

--- R/domainEnrichment.R
+++ R/domainEnrichment.R
@@ -1,12 +1,16 @@
 # domainEnrichment.R
 
-#' Domain enrichment in a system.
+#' Domain enrichment in a desired system.
 #'
 #' \code{domainEnrichment} Find the enriched domains in selected system and interpret enrichment by plot.
 #'
-#' @param system The shortehand notation of selected system, five-letter code as discussed in class. Default is PHALY.
-#' @param numEnrichment The number of enriched domain ploted. Default is 40.
-#' @return A data frame that include domains in the system with its counts and frequency of exist in system and in random genes; p-value, odd ratio, adjust p-value and enrichment; interpro descriptions.
+#' For the PHYLA system, see details in the \href{../doc/domainEnrichment-Vignette.html}{PHALY domain enrichemnt vignette} (or load the vignette with \code{vignette("domainEnrichment-Vignette", package = "BCB420.2019.ESA")}).
+#'
+#' @section Details: This function is created as part of BCB420 2019 winter course. The domain databaase were fetched from InterPro, see more details in \code{\link{fetchData}}.
+#'
+#' @param system The shortehand notation of selected system, five-letter code as discussed in class.
+#' @param alpha The alpha value for Benjamini-Hochberg control. Default is 0.05.
+#' @return A data frame that include domains in the system with its counts and frequency in system and in random genes; p-value, odd ratio, adjust p-value and enrichment; interpro descriptions. Return NULL if system is not available by \code{\link{SyDBgetSysSymbols}}.
 #'
 #' @import stats
 #' @import biomaRt
@@ -15,28 +19,36 @@
 #'
 #' @author \href{https://orcid.org/0000-0001-8720-5874}{Fan Shen} (aut)
 #'
+#' @seealso \code{\link{fetchData}} Fetch data from package
+#' @seealso \code{\link{SyDBgetRootSysIDs}} Get the system ID in database
+#' @seealso \code{\link{SyDBgetSysSymbols}} Get the genes in desired system
+#'
 #' @examples
-#' # Plot 50 enrichend domains in PHALY system orderd by rich factor(counts in system/counts in random)
-#' # User can hover around the point on plot to get more info (i.e counts, p-value)
+#' # Plot enrichend domains in PHALY system orderd by rich factor
+#' # Plot would separate to two parts if too many domains are enriched
+#' # User can hover around the point on plot to get more info (i.e ID descripton)
 #' system <- "PHALY"
 #' domainEnrichment(system)
+#'
 #' @export
-domainEnrichment <- function(system = "PHALY", numEnrichment = 40) {
+domainEnrichment <- function(system, alpha = 0.05) {
   # 1. Fetch data from Dr. Steipe's BCB420.2019.ESA package
-  # Genes and their domains
+  # Two lists from InterPro database
   genesIPR <- fetchData("genesIPR")
-
-  # domains and their genes
   IPRgenes <- fetchData("IPRgenes")
 
-  # Get the genes for the system
+  # Genes for the system
   myDB <- fetchData("SysDB")
-  genes <- SyDBgetSysSymbols(myDB, system)
-  #genes <- fetchComponents(system)
+  if (! system %in% names(SyDBgetRootSysIDs(myDB))) {
+    print("Input system should be in database of ESA package.")
+    return(NULL)
+  }
+
+  genes <- SyDBgetSysSymbols(myDB, system)[[1]]
 
 
-  # 2. Find the enrichment counts
-  # 2.1 store all the domains that in the system and their counts
+  # 2. Find the counts of genes for each domain
+  # 2.1 Store all the domains that in the system and their counts
   domains <- c()
   for (gene in genes) {
     domains <- c(genesIPR[gene][[1]], domains)
@@ -45,93 +57,108 @@ domainEnrichment <- function(system = "PHALY", numEnrichment = 40) {
   tmp <- as.data.frame(table(domains))
   colnames(tmp) <- c("domains", "sysCounts")
 
-  # Store the contingency matrix for each gene
-  contingency <- list()
+  # 2.2 Store the counts from random (all genes)
   for(i in seq_along(tmp$domains)) {
-    randomCount <- length(IPRgenes[as.character(tmp$domains[i])][[1]])
-    tmp$randomCounts[i] <- randomCount
-    contingency[[i]] <- c(tmp$sysCounts[i],
-      randomCount,
-      sum(genes %in% names(genesIPR))-tmp$sysCounts[i],
-      length(genesIPR)-randomCount)
-    names(contingency)[i] <- as.character(tmp$domains[i])
+    tmp$randomCounts[i] <- length(IPRgenes[as.character(tmp$domains[i])][[1]])
   }
 
-  # 2.2 find the enrichment for each domain in the system
+
+  # 3. Data analysis
+  # 3.1 Calculate the frequency for each domain in the system
   tmp$sysFreq <- (tmp[,2]) / (sum(genes %in% names(genesIPR)))
 
-  # 2.3 find the enrichment for each domain in all genes
+  # 3.2 Calculate the frequency for each domain in random (all genes)
   tmp$randomFreq <- tmp$randomCounts/length(genesIPR)
 
+  # 3.3 Calculate the p_value and odd_ratio
+  # Null hypothesis: The categories are independent.
+  # Some useful page for understanding recommended by Dr. Steipe.
+  # https://www.pathwaycommons.org/guide/primers/statistics/fishers_exact_test/
+  # https://www.pathwaycommons.org/guide/primers/statistics/multiple_testing/
 
-  # 3. Calculate the p_value for enrichment
-  fisherTests <- data.frame(domain = character(),
-                            p_value = numeric(),
-                            odd_ratio = numeric(),
-                            stringsAsFactors = FALSE)
+  # Fisher Test Ref:
+  # M. Berk (https://stats.stackexchange.com/users/30201/m-berk), Which
+  # statistical test should be used to test for enrichment of gene lists?,
+  # URL (version: 2013-10-11): https://stats.stackexchange.com/q/72556
+  for (i in seq_along(tmp$domains)) {
+    contingency <- c(tmp$sysCounts[i],
+                     tmp$randomCounts[i],
+                     sum(genes %in% names(genesIPR))-tmp$sysCounts[i],
+                     length(genesIPR)-tmp$randomCounts[i])
 
-  for (i in seq_along(contingency)) {
-    fisherTest <- fisher.test(matrix(contingency[[i]],nrow=2,ncol=2),alternative="greater")
-    add <- data.frame(domain = names(contingency)[i],
-                      p_value = fisherTest$p.value,
-                      odd_ratio = fisherTest$estimate,
-                      row.names = i,
-                      stringsAsFactors = FALSE)
-    fisherTests[i,] <- add
+    fisherTest <- fisher.test(matrix(contingency, nrow=2, ncol=2),
+                                     alternative="greater")
+
+    tmp$p_value[i] <- fisherTest$p.value
+    tmp$odd_ratio[i] <- fisherTest$estimate
   }
 
-  fisherTests$p_adj <- stats::p.adjust(fisherTests$p_value)
+  # 3.4 Multiple testing: Benjamini-Hochberg control for false discovery rate
+  tmp$p_adj_BH <- stats::p.adjust(tmp$p_value, "BH")
 
-  # Put all analyzed stats together
+  # 3.5 Calculate the enrichment
   tmp$enrichment <- (tmp$sysFreq)/(tmp$randomFreq)
-  tmp <- merge(tmp, fisherTests, by.x = "domains", by.y = "domain")
+
 
   # 4. Find the description for enriched domains
   myMart <- biomaRt::useMart("ensembl", dataset="hsapiens_gene_ensembl")
 
-  filters <- biomaRt::listFilters(myMart)
-  attributes <- biomaRt::listAttributes(myMart)
-
   description <- biomaRt::getBM(filters = "interpro",
-    attributes = c("interpro",
-      "interpro_short_description",
-      "interpro_description"),
-    values = tmp$domains,
-    mart = myMart)
+                                attributes = c("interpro",
+                                               "interpro_short_description",
+                                               "interpro_description"),
+                                values = tmp$domains,
+                                mart = myMart)
 
-  # Merge the two data frame, such that those with no description are excluded since they are family
+  # Merge the two data frame
+  # note: domain IDs with no description are excluded since they are family
   tmp <- merge(tmp, description, by.x = "domains", by.y = "interpro")
 
-  # Reorder the tmp by enrichment
-  tmp <- tmp[order(tmp$enrichment,decreasing = TRUE),]
 
-  # Multiple testing?
+  # 5. plot
+  # 5.1 Get rid of false discovery.
+  # Only tmp$p_adj_BH <= alpha. Small p-value means reject null hypothesis.
+  output <- tmp[tmp$p_adj_BH <= alpha, ]
 
-  output <- tmp[1:numEnrichment,]
-  output$tooltip <- c(paste0("Name = ", output$interpro_short_description,
+  # 5.2 Add output text for each domain
+  output$tooltip <- c(paste0("ID = ", output$domains,
+                             "\n Name = ", output$interpro_short_description,
                              "\n Counts in sys = ", output$sysCounts,
                              "\n Counts in random = ", output$randomCounts,
                              "\n p-value = ", output$p_value,
                              "\n odd ratio = ", output$odd_ratio,
+                             "\n p-value BH = ", output$p_adj_BH,
                              "\n Enrichment = ", output$enrichment))
-  # 5. Plot
-  p1 <- ggplot(output, aes(x = output$sysCounts/output$randomCounts, y = reorder(output$interpro_short_description, output$sysCounts/output$randomCounts)))
-  my_gg <- p1 +
-    geom_point_interactive(tooltip = output$tooltip, size=2.5) +
-    geom_point(aes(color=output$p_value)) +
-    xlab("Rich Factor") +
-    ylab("Interpro Description") +
-    ggtitle(paste0("Enriched Domains in ", system))
+  output <- output[order(output$sysCounts/output$randomCounts, decreasing = TRUE), ]
+
+  # 5.3 Plot the data
+  if (nrow(output) > 40) {
+    separate = ceiling(nrow(output)/2)
+    for (i in seq(2)) {
+      start <- (i-1)*separate+1
+      output$label[start:nrow(output)] <- rep(paste0("Part ",i), nrow(output)-start+1)
+    }
+  } else {
+    output$label <- rep("",nrow(output))
+  }
+
+  p <- ggplot(output,
+               aes(x = output$sysCounts/output$randomCounts,
+                   y = reorder(output$domains,
+                               output$sysCounts/output$randomCounts)),scales = "free_y")
+  my_gg <- p +
+           geom_point_interactive(tooltip = output$tooltip, size=2.5) +
+           geom_point(aes(color=output$p_value)) +
+           labs(colour="P value")+
+           xlab("Rich Factor") +
+           ylab("Interpro ID") +
+           ggtitle(paste0("Enriched Domains in ", system)) +
+           theme(axis.text.y = element_text(angle = 20, hjust = 1)) +
+           facet_wrap(~output$label, scales = "free")
 
   print(girafe(code = print(my_gg)))
 
   return(tmp)
-
-  # TODO: Wrap up
-  # describe the example a little bit!
-  # use devtools::document() to get the namespace and md file
-  # Add pkg for description
-  # devtools::use_package("biomaRt") to add pkg
 }
 
 # [END]
