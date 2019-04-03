0e784a0eb7b591d65f0c2fdfe0db42e2406c6a65
Yeon Joo Cha <cathy455503@gmail.com>
2019-04-02 23:22:55 -0400

    added helper function for human gene annotation database import

---
+++ R/HuGenAnnotImport.R
@@ -0,0 +1,18 @@
+# HuGenAnnotImport.R
+
+#' \code{HuGenAnnotImport} imports the org.Hs.eg.db, implemented to reduce import burden of package
+#' This database will only be imported when the semanticSimilarity.R function is required
+#'
+#' @return NULL
+#' @examples
+#' \dontrun{
+#' HuGenAnnotImport()
+#' }
+#' @export
+
+HuGenAnnotImport <- function() {
+  if (! requireNamespace("org.Hs.eg.db")) {
+    BiocManager::install("org.Hs.eg.db")
+  }
+  return(invisible(NULL))
+}
